3b71837816beed2d8446bf5d237c0c258febbeb9
Ahmed Hisham <ahmed.hisham@advansys-esc.com>
2018-07-17 13:31:58 +0200

    updated api-gateway-security , made it check test.properties for logging

--- src/main/resources/scripts/UsersSchemaUpdate.sql
+++ src/main/resources/scripts/UsersSchemaUpdate.sql
@@ -1,3 +1,5 @@
+#11-June-2018
+
 INSERT INTO `lms_users`.`permission` (`id`, `name`) VALUES ('46', 'APPROVE_SUBSCRIPTION');
 
 INSERT INTO `lms_users`.`role_permission` (`role_id`, `permission_id`) VALUES ('1', '46');
